4837d738f362afa8dbf8ad3ae75072063e0685a2
ALBENA JOHN <amrutha4325@gmail.com>
2026-07-24 09:15:48 +0530

    Implement MyHashSet with basic operations

---
+++ 705-Design hash set.java
@@ -0,0 +1,32 @@
+class MyHashSet {
+    private List<Integer>[] buckets;
+    private int size = 1000;
+
+    public MyHashSet() {
+        buckets = new List[size];
+        for (int i = 0; i < size; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    private int hash(int key) {
+        return key % size;
+    }
+
+    public void add(int key) {
+        int index = hash(key);
+        if (!buckets[index].contains(key)) {
+            buckets[index].add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int index = hash(key);
+        buckets[index].remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        int index = hash(key);
+        return buckets[index].contains(key);
+    }
+}
